66f67728e4753e9cb659b38e6ce57fddb1104a65
Adidi0830 <muhammadadialsaadan@gmail.com>
2025-10-27 20:59:49 +0700

    Masukkan foto baru

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ export default function Hero() {
   return (
     <section className="text-center py-20 bg-gray-100">
       <h1 className="text-5xl font-bold mb-4">Halo, Saya Muhammad Adi Al Saadan 👋</h1>
+      <center><img src="/public/Gambar WhatsApp 2025-10-27 pukul 20.53.41_1fa58d35.jpg" ></img></center>
       <p className="text-lg text-gray-600">Siswa • Entry Level Programmer • Pembelajar Seumur Hidup</p>
       <a href="#projects" className="mt-6 inline-block bg-black text-yellow-400 px-6 py-2 rounded-full hover:bg-yellow-500 hover:text-black transition">
         Lihat Proyek Saya
