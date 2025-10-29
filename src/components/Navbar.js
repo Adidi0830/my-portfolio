2b45832174becce9ba02051bf53893010dad78c5
Adidi0830 <muhammadadialsaadan@gmail.com>
2025-10-29 07:34:59 +0700

    Edit Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="bg-black text-white py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <h1 className="text-xl font-bold">Saadan's Portfolio</h1>
+        <h1 className="text-xl font-bold">My Portfolio</h1>
         <ul className="flex gap-6">
           <li><a href="#projects" className="hover:text-yellow-400">Projects</a></li>
           <li><a href="#contact" className="hover:text-yellow-400">Contact</a></li>
