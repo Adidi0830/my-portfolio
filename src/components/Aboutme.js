0603d1fac7e0f85929ae04400f09e5fb7e5055fc
Adidi0830 <muhammadadialsaadan@gmail.com>
2025-10-31 20:38:36 +0700

    Edit ulang Aboutme.js dan menambahkan perintah baru di App.css

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -2,13 +2,13 @@ export default function Aboutme() {
   return (
     <section id="aboutme" className="py-20 bg-white text-center max-w-3xl mx-auto rounded-lg shadow-md px-6">
       <h2 className="text-3xl font-bold mb-6">About Me</h2>
-      <p className="text-gray-700 mb-4">
-        Hai! Aku Muhammad Adi Al Saadan, siswa SMKN 1 Probolinggo jurusan RPL dan menjadi bagian dari Axioo Class Program. Sekarang fokus memperdalam skill pengembangan perangkat lunak lewat IDCamp Dicoding di bidang Data Science.
+      <p className="text-gray-700 mb-4 text-justify">
+        Hai! Aku Muhammad Adi Al Saadan, siswa SMKN 1 Probolinggo jurusan RPL dan alumni Axioo Class Program. Sekarang fokus memperdalam skill pengembangan perangkat lunak lewat IDCamp Dicoding.
       </p>
-      <p className="text-gray-700 mb-4">
+      <p className="text-gray-700 mb-4 text-justify">
         Passion-ku di database, web, dan sistem. Cukup mahir PHP dan HTML, biasa bikin project web yang rapi dan fungsional.
       </p>
-      <p className="text-gray-700">
+      <p className="text-gray-700 text-justify">
         Buatku, setiap baris kode itu langkah nyata buat wujudin ide.
       </p>
     </section>
