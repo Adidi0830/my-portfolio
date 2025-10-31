572efff5e6cd68b26a53486b3932545794186b78
Adidi0830 <muhammadadialsaadan@gmail.com>
2025-10-31 21:02:29 +0700

    Edit Aboutme.js dan mengembalikan App.js ke setelan awal

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,6 +1,6 @@
 export default function Aboutme() {
   return (
-    <section id="aboutme" className="py-20 bg-white text-center max-w-3xl mx-auto rounded-lg shadow-md px-6">
+    <section id="aboutme" className="py-20 bg-white text-center max-w-4xl mx-auto px-10">
       <h2 className="text-3xl font-bold mb-6">About Me</h2>
       <p className="text-gray-700 mb-4 text-justify">
         Hai! Aku Muhammad Adi Al Saadan, siswa SMKN 1 Probolinggo jurusan RPL dan alumni Axioo Class Program. Sekarang fokus memperdalam skill pengembangan perangkat lunak lewat IDCamp Dicoding.
